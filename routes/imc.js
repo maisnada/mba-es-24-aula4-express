e64b9e4d2520bb6f96462279c33a18d441a27fe3
Marcelo Moreno <marcelo.moreno@univali.br>
2023-03-21 22:10:45 -0300

    ajuste equipe

--- routes/imc.js
+++ routes/imc.js
@@ -2,23 +2,23 @@ var express = require("express");
 var router = express.Router();
 
 function calcularImc(dados) {
-  let imc = dados.peso / dados.altura ** 2;
+  let imc = dados.weight / dados.height ** 2;
 
   dados.imc = Number(imc.toFixed(2));
 }
 
 function descricaoImc(dados) {
-  dados.descricao = "obesidade";
+  dados.imcDescription = "obesidade";
 
-  if (dados.imc < 18.5) dados.descricao = "magreza";
-  if (dados.imc >= 18.5 && dados.imc < 24.9) dados.descricao = "normal";
-  if (dados.imc >= 24.9 && dados.imc < 30) dados.descricao = "sobrepeso";
+  if (dados.imc < 18.5) dados.imcDescription = "magreza";
+  if (dados.imc >= 18.5 && dados.imc < 24.9) dados.imcDescription = "normal";
+  if (dados.imc >= 24.9 && dados.imc < 30) dados.imcDescription = "sobrepeso";
 }
 
 router.post("/", function (req, res, next) {
-  const { altura, peso } = req.body;
+  const { height, weight } = req.body;
 
-  let dados = { altura, peso };
+  let dados = { height, weight };
 
   calcularImc(dados);
   descricaoImc(dados);
